43d8f55cacf0e23b4cda7f711390d013b17d189d
Tara Roskell <tara@roskelldesign.co.uk>
2013-05-09 12:40:19 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogwalksproject::Application.config.secret_token = '15b2fa1ba55484ad5ed6d99bca8abd16aee07c40411207984323a95994a73ee37f9d7a15ec1ed951eb07c13e096510fc6afd094e36c9973804c70bd46512f428'
