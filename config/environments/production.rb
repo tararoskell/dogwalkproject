ba2de00b8594da101b1c6761ba7e6cc844abe634
Tara Roskell <tara@roskelldesign.co.uk>
2013-05-16 13:56:34 +0100

    add Amazon S3 gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,18 @@ Dogwalksproject::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  
+   # In production, :host should be set to the actual host of your application.
    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
- # In production, :host should be set to the actual host of your application.
+# Configure paperclip for Amazon S3 Upload
+ 
+   config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
